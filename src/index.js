adff4188ad64a358225c7485e6f605bb396924df
Deepak <deepakworkpc@gmail.com>
2024-06-19 00:20:31 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
+import './style.css';
 
 import myName from './myName';
 
 function component() {
-    const element = document.createElement('div');
-  
+  const element = document.createElement('div');
 
-    element.textContent = myName("Shivam");
-    return element;
-  }
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ element.classList.add('hello');
+
+  return element;
+ }
   
   document.body.appendChild(component());
   index.html
